cedd7aa088e7be9caa9d3f3f407f462a3e83df53
William Mian <william_mian@yahoo.com.br>
2019-02-12 14:10:51 -0300

    api error

--- src/main/java/com/hibicode/beerstore/model/Beer.java
+++ src/main/java/com/hibicode/beerstore/model/Beer.java
@@ -25,14 +25,14 @@ public class Beer {
 	@EqualsAndHashCode.Include
     private Long id;
 
-	@NotBlank
+	@NotBlank(message = "beers-1")
     private String name;
 
-	@NotNull
+	@NotNull(message = "beers-2")
     private BeerType type;
 
-	@NotNull
-	@DecimalMin("0")
+	@NotNull(message = "beers-3")
+	@DecimalMin(value = "0", message = "beers-4")
     private BigDecimal volume;
 
 }
