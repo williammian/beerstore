0088616baf352f659a60f137db9f2ab0b7a5c25e
willi <willi@LAPTOP-B6ER7NR3>
2019-02-13 06:45:20 -0200

    tratando erros inesperados

--- src/main/java/com/hibicode/beerstore/error/ApiExceptionHandler.java
+++ src/main/java/com/hibicode/beerstore/error/ApiExceptionHandler.java
@@ -54,6 +54,15 @@ public class ApiExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handlerInternalServerError(Exception exception, Locale locale) {
+        LOG.error("Error not expected", exception);
+        final String errorCode = "error-1";
+        final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        final ErrorResponse errorResponse = ErrorResponse.of(status, toApiError(errorCode, locale));
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+    
     private ApiError toApiError(String code, Locale locale, Object... args) {
         String message;
         try {
