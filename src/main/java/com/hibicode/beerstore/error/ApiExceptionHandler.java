1d3d6dca8cb8bd437868220cb75b81d16572790e
willi <willi@LAPTOP-B6ER7NR3>
2019-02-13 07:07:49 -0200

    tratando erros

--- src/main/java/com/hibicode/beerstore/error/ApiExceptionHandler.java
+++ src/main/java/com/hibicode/beerstore/error/ApiExceptionHandler.java
@@ -10,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -22,6 +24,7 @@ import com.hibicode.beerstore.error.ErrorResponse.ApiError;
 
 import lombok.RequiredArgsConstructor;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ApiExceptionHandler {
@@ -53,17 +56,8 @@ public class ApiExceptionHandler {
         final ErrorResponse errorResponse = ErrorResponse.of(status, toApiError(errorCode, locale, exception.getValue()));
         return ResponseEntity.badRequest().body(errorResponse);
     }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handlerInternalServerError(Exception exception, Locale locale) {
-        LOG.error("Error not expected", exception);
-        final String errorCode = "error-1";
-        final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        final ErrorResponse errorResponse = ErrorResponse.of(status, toApiError(errorCode, locale));
-        return ResponseEntity.status(status).body(errorResponse);
-    }
     
-    private ApiError toApiError(String code, Locale locale, Object... args) {
+    public ApiError toApiError(String code, Locale locale, Object... args) {
         String message;
         try {
             message = apiErrorMessageSource.getMessage(code, args, locale);
