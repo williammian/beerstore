d0902ca89ecc408c3754a400a3356a08684c8d7b
William Mian <william_mian@yahoo.com.br>
2019-02-14 13:35:11 -0300

    adicionando o repositorio no servico

--- src/main/java/com/hibicode/beerstore/repository/Beers.java
+++ src/main/java/com/hibicode/beerstore/repository/Beers.java
@@ -1,11 +1,14 @@
 package com.hibicode.beerstore.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.model.BeerType;
 
 public interface Beers extends JpaRepository<Beer, Long> {
 
-    
+    Optional<Beer> findByNameAndType(String name, BeerType type);
 
 }
--- src/main/java/com/hibicode/beerstore/service/BeerService.java
+++ src/main/java/com/hibicode/beerstore/service/BeerService.java
@@ -1,12 +1,28 @@
 package com.hibicode.beerstore.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.repository.Beers;
 import com.hibicode.beerstore.service.exception.BeerAlreadyExistException;
 
+@Service
 public class BeerService {
 	
+	@Autowired
+	private Beers beers;
+	
 	public Beer save(final Beer beer) {
-		throw new BeerAlreadyExistException();
+		Optional<Beer> beerByNameAndType = beers.findByNameAndType(beer.getName(), beer.getType());  
+		
+		if(beerByNameAndType.isPresent()) {
+			throw new BeerAlreadyExistException();
+		}
+		
+		return beers.save(beer);
 	}
 
 }
