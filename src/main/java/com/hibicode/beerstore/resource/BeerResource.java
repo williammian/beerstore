7a5e91ed7cdba7398180dee2a7681b1d2163d325
William Mian <william_mian@yahoo.com.br>
2019-02-15 08:53:04 -0300

    endpoint da edicao

--- src/main/java/com/hibicode/beerstore/resource/BeerResource.java
+++ src/main/java/com/hibicode/beerstore/resource/BeerResource.java
@@ -7,7 +7,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,4 +39,10 @@ public class BeerResource {
 	public Beer create(@Valid @RequestBody Beer beer) {
 		return beerService.save(beer);
 	}
+	
+	@PutMapping("/{id}")
+    public Beer update(@PathVariable Long id, @Valid @RequestBody Beer beer) {
+        beer.setId(id);
+        return beerService.save(beer);
+    }
 }
