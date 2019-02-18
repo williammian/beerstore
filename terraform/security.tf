041b3fb03357934d69355385f00f2c1fb0187c7b
William Mian <william_mian@yahoo.com.br>
2019-02-18 15:36:43 -0300

    terraform

--- terraform/security.tf
+++ terraform/security.tf
@@ -9,3 +9,15 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["191.205.121.137/32"]
   }
 }
+
+resource "aws_security_group" "database" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "wm_database"
+
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    self = true
+  }
+}
