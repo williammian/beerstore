ce6140362497264823b8a3d1b25237699463b4ad
William Mian <william_mian@yahoo.com.br>
2019-02-18 13:47:23 -0300

    terraform

---
+++ terraform/instances.tf
@@ -0,0 +1,22 @@
+resource "aws_key_pair" "keypair" {
+  public_key = "${file("key/beerstore_key.pub")}"
+}
+
+resource "aws_instance" "instances" {
+  count = 3
+
+  ami = "ami-035be7bafff33b6b6"
+  instance_type = "t2.micro"
+
+  subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  
+  key_name = "${aws_key_pair.keypair.key_name}"
+
+  tags {
+    Name = "wm_instances"
+  }
+}
+
+output "public_ips" {
+  value = "${join(", ", aws_instance.instances.*.public_ip)}"
+}
--- terraform/network.tf
+++ terraform/network.tf
@@ -5,3 +5,30 @@ resource "aws_vpc" "main" {
     Name = "wm"
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  count = 3
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 10)}"
+  availability_zone = "${var.availability_zones[count.index]}"
+
+  tags {
+    Name = "wm_private_subnet_${count.index}"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = 3
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 20)}"
+  availability_zone = "${var.availability_zones[count.index]}"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "wm_public_subnet_${count.index}"
+  }
+}
